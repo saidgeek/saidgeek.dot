3e9182d043205d36651c2cce1fc3f18812977bd5
saidgeek <saidgeek@Saidgeek-Mac-mini.local>
2025-08-26 12:17:58 -0400

    fix: mason error by v2

---
+++ nvim/lua/plugins/mason-workaround.lua
@@ -0,0 +1,5 @@
+-- This resolve a poblem, link issue: https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
